5314e16e124640ec35ef1b0bba15c02e648962eb
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-02-16 08:48:43 +0200

    classwork

--- lesson1/homework/app.js
+++ lesson1/homework/app.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
 const path = require("path");
 
-// fs.mkdir(path.join(__dirname, 'main'), (err) => {
-//     if (err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-// fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
-//     if (err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-// fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
-//     if (err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
+fs.mkdir(path.join(__dirname, 'main'), (err) => {
+    if (err){
+        console.log(err);
+        throw err;
+    }
+})
+fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
+    if (err){
+        console.log(err);
+        throw err;
+    }
+})
+fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
+    if (err){
+        console.log(err);
+        throw err;
+    }
+})
 
 let onlineUsers = [
     {name: 'Kolia', age: 15, city: 'Kyiv'},
@@ -31,25 +31,27 @@ let inPersonUsers = [
     {name: 'Dima', age: 19, city: 'Brody'}
 ];
 
+
 const users = onlineUsers.map(user => `\nName: ${user.name} Age: ${user.age} City:${user.city}`);
-//
-// fs.writeFile(path.join(__dirname, 'main', 'online', 'onlineUser.txt'),
-//      `${users}`, (err) => {
-//     if (err){
-//         console.log(err);
-//         throw err;
-//     }
-// })
-//
+
+fs.writeFile(path.join(__dirname, 'main', 'online', 'onlineUser.txt'),
+     `${users}`, (err) => {
+    if (err){
+        console.log(err);
+        throw err;
+    }
+})
+
+
 const persons = inPersonUsers.map(person => `\nName: ${person.name} Age: ${person.age} City: ${person.city}` );
-//
-// fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'inPersonUser.txt'),
-//     `${persons}`, (err) => {
-//         if (err){
-//             console.log(err);
-//             throw err;
-//         }
-//     })
+
+fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'inPersonUser.txt'),
+    `${persons}`, (err) => {
+        if (err){
+            console.log(err);
+            throw err;
+        }
+    })
 
 function substitute(online, inPerson) {
     fs.appendFile(path.join(__dirname, 'main', 'online', 'onlineUser.txt'), `${inPerson}`,
@@ -58,7 +60,6 @@ function substitute(online, inPerson) {
             console.log(err);
             throw err
         }
-        console.log(inPerson)
     })
     fs.appendFile(path.join(__dirname, 'main', 'inPerson', 'inPersonUser.txt'), `${online}`,
         {flag: 'w'},(err) => {
@@ -67,7 +68,6 @@ function substitute(online, inPerson) {
                 throw err
             }
         })
-    console.log(online)
 }
 
 substitute(users, persons);
