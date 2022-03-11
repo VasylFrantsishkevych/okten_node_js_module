06127401bbc340737def122019a47fa73c9a0c2f
Vasyl Frantsishkevych <91376102+VasylFrantsishkevych@users.noreply.github.com>
2022-03-11 14:07:35 +0200

    Delete dist directory

--- dist/router/apiRouter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.apiRouter = void 0;
-const express_1 = require("express");
-const userRouter_1 = require("./userRouter");
-const router = (0, express_1.Router)();
-router.use('/users', userRouter_1.userRouter);
-exports.apiRouter = router;
-//# sourceMappingURL=apiRouter.js.map
