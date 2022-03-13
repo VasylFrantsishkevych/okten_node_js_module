186b34496c9cfa2b633a8ee314266d6ea88d4285
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-03-13 12:45:53 +0200

    lesson 5

--- dist/app.js
+++ dist/app.js
@@ -16,13 +16,109 @@ require("reflect-metadata");
 const express_1 = __importDefault(require("express"));
 const typeorm_1 = require("typeorm");
 const user_1 = require("./entity/user");
+const post_1 = require("./entity/post");
+const comment_1 = require("./entity/comment");
 const app = (0, express_1.default)();
-// app.use(express.json());
-// app.use(express.urlencoded());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded());
+// app.get('/users', async (req: Request, res: Response) => {
+//     const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+//
+//     res.json(users);
+//     // const users = await getManager().getRepository(User).findOne();
+//     // console.log(users);
+//     // res.json(users);
+// });
+// Get all users
 app.get('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const users = yield (0, typeorm_1.getManager)().getRepository(user_1.User).find();
-    console.log(users);
+    const users = yield (0, typeorm_1.getManager)().getRepository(user_1.User)
+        .createQueryBuilder('user')
+        .leftJoin('Post', 'post', 'post.userId = user.id')
+        .getMany();
     res.json(users);
+}));
+// Create User
+app.post('/users', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const createUser = yield (0, typeorm_1.getManager)().getRepository(user_1.User).save(req.body);
+    res.json(createUser);
+}));
+// Get user by id
+app.get('/users/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    const user = yield (0, typeorm_1.getManager)().getRepository(user_1.User)
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id: Number(id) })
+        .leftJoin('Post', 'posts', 'posts.userId = user.id')
+        .getOne();
+    res.json(user);
+}));
+// Update field password and email in user
+app.patch('/users/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { password, email } = req.body;
+    const createdUser = yield (0, typeorm_1.getManager)().getRepository(user_1.User)
+        .update({ id: +req.params.id }, {
+        password,
+        email,
+    });
+    res.json(createdUser);
+}));
+// Delete user by id
+app.delete('/users/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const deleteUser = yield (0, typeorm_1.getManager)().getRepository(user_1.User)
+        .softDelete({ id: Number(req.params.id) });
+    res.json(deleteUser);
+}));
+// Create post
+app.post('/posts', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const post = yield (0, typeorm_1.getManager)().getRepository(post_1.Post).save(req.body);
+        res.status(201).json(post);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}));
+// Get post by userId
+app.get('/posts/:userId', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { userId } = req.params;
+    const post = yield (0, typeorm_1.getManager)().getRepository(post_1.Post)
+        .createQueryBuilder('post')
+        .where('post.userId = :id', { id: Number(userId) })
+        .leftJoin('User', 'user', 'user.id = post.userId')
+        .getMany();
+    res.json(post);
+}));
+// Update fields title and text in post
+app.patch('/posts/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { title, text } = req.body;
+    const { id } = req.params;
+    const updatePost = yield (0, typeorm_1.getManager)().getRepository(post_1.Post)
+        .update({ id: Number(id) }, {
+        title,
+        text,
+    });
+    res.json(updatePost);
+}));
+// Create comments
+app.post('/comments', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const createComment = yield (0, typeorm_1.getManager)().getRepository(comment_1.Comment).save(req.body);
+    res.json(createComment);
+}));
+// Get comments to user and post which they write
+app.get('/comments/:id', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const comments = yield (0, typeorm_1.getManager)().getRepository(comment_1.Comment)
+            .createQueryBuilder('comment')
+            .where('comment.authorId = :id', { id: Number(id) })
+            .leftJoinAndSelect('comment.user', 'user')
+            .leftJoinAndSelect('comment.post', 'post')
+            .getMany();
+        res.json(comments);
+    }
+    catch (e) {
+        console.log(e);
+    }
 }));
 app.listen(5500, () => __awaiter(void 0, void 0, void 0, function* () {
     console.log('Server has started!!!');
--- src/app.ts
+++ src/app.ts
@@ -2,39 +2,154 @@ import 'reflect-metadata';
 import express, { Request, Response } from 'express';
 import { createConnection, getManager } from 'typeorm';
 import { User } from './entity/user';
+import { Post } from './entity/post';
+import { Comment } from './entity/comment';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/users', async (req: Request, res: Response) => {
-    const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+// app.get('/users', async (req: Request, res: Response) => {
+//     const users = await getManager().getRepository(User).find({ relations: ['posts'] });
+//     res.json(users);
+//     // const users = await getManager().getRepository(User).findOne();
+//     // console.log(users);
+//     // res.json(users);
+// });
 
+// Get all users
+app.get('/users', async (req: Request, res: Response) => {
+    const users = await getManager().getRepository(User)
+        .createQueryBuilder('user')
+        .leftJoin('Post', 'posts', 'posts.userId = user.id')
+        .getMany();
     res.json(users);
-    // const users = await getManager().getRepository(User).findOne();
-    // console.log(users);
-    // res.json(users);
 });
 
+// Create User
 app.post('/users', async (req: Request, res: Response) => {
     const createUser = await getManager().getRepository(User).save(req.body);
     res.json(createUser);
 });
 
-app.put('/users/:id', async (req, res) => {
+// Get user by id
+app.get('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const user = await getManager().getRepository(User)
+        .createQueryBuilder('user')
+        .where('user.id = :id', { id: Number(id) })
+        .leftJoin('Post', 'posts', 'posts.userId = user.id')
+        .getOne();
+    res.json(user);
+});
+
+// Update field password and email in user
+app.patch('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
     const { password, email } = req.body;
     const createdUser = await getManager().getRepository(User)
-        .update({ id: Number(req.params.id) }, {
+        .update({ id: +id }, {
             password,
             email,
         });
     res.json(createdUser);
 });
 
-app.delete('/users/:id', async (req, res) => {
+// Delete user by id
+app.delete('/users/:id', async (req: Request, res: Response) => {
+    const { id } = req.params;
     const deleteUser = await getManager().getRepository(User)
-        .delete({ id: Number(req.params.id) });
+        .softDelete({ id: +id });
     res.json(deleteUser);
+});
+
+// Create post
+app.post('/posts', async (req: Request, res: Response) => {
+    try {
+        const post = await getManager().getRepository(Post).save(req.body);
+        res.status(201).json(post);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// Get all posts
+app.get('/posts', async (req: Request, res: Response) => {
+    const users = await getManager().getRepository(Post)
+        .createQueryBuilder()
+        .getMany();
+    res.json(users);
+});
+
+// Get post by userId
+app.get('/posts/:userId', async (req: Request, res: Response) => {
+    const { userId } = req.params;
+    const post = await getManager().getRepository(Post)
+        .createQueryBuilder('post')
+        .where('post.userId = :id', { id: Number(userId) })
+        .leftJoin('User', 'user', 'user.id = post.userId')
+        .getMany();
+    res.json(post);
+});
+
+// Update fields title and text in post
+app.patch('/posts/:id', async (req: Request, res: Response) => {
+    const { title, text } = req.body;
+    const { id } = req.params;
+    const updatePost = await getManager().getRepository(Post)
+        .update({ id: Number(id) }, {
+            title,
+            text,
+        });
+    res.json(updatePost);
+});
+
+// Create comments
+app.post('/comments', async (req: Request, res: Response) => {
+    const createComment = await getManager().getRepository(Comment).save(req.body);
+    res.json(createComment);
+});
+
+// Get comments to user and post which they write
+app.get('/comments/:id', async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const comments = await getManager().getRepository(Comment)
+            .createQueryBuilder('comment')
+            .where('comment.authorId = :id', { id: Number(id) })
+            .leftJoinAndSelect('comment.user', 'user')
+            .leftJoinAndSelect('comment.post', 'post')
+            .getMany();
+        res.json(comments);
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+// Update fields like and dislike in comments
+app.post('/comments/action', async (req: Request, res: Response) => {
+    try {
+        const { action, commentId } = req.body;
+        const queryRunner = getManager().getRepository(Comment);
+        const comment = await queryRunner.createQueryBuilder('comment')
+            .where('comment.id = :id', { id: commentId })
+            .getOne();
+
+        if (!comment) {
+            throw new Error('wrong comment ID');
+        }
+
+        if (action === 'like') {
+            await queryRunner.update({ id: commentId }, { like: comment.like + 1 });
+        }
+        if (action === 'dislike') {
+            await queryRunner.update({ id: commentId }, { dislike: comment.dislike + 1 });
+        }
+
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+    }
 });
 
 app.listen(5500, async () => {
